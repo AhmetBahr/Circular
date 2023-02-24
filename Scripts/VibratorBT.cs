44dd00769a5d96a3237b77564bba2dd8f61fd057
Ahmet.B <ahmetbhr29@gmail.com>
2023-02-24 12:37:36 +0300

    Circular save2

---
+++ Scripts/VibratorBT.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VibratorBT : MonoBehaviour
+{
+    [SerializeField] Image vibraOn;
+    [SerializeField] Image vibraOff , vibraOff_Line;
+    public bool vibra = false;
+    public int vibraInt;
+
+    void Start()
+    {
+       
+        if (!PlayerPrefs.HasKey("vibra"))
+        {
+            PlayerPrefs.SetInt("vibra", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        UpdateButtonIcon();
+    }
+    public void OnButtonPress()
+    {
+        if (vibra == false)
+        {
+            vibra = true;
+       //     vibraInt = 1;
+        }
+        else
+        {
+            vibra = false;
+        //    vibraInt= 0;
+        }
+        Save();
+        UpdateButtonIcon();
+    }
+
+
+    private void Load()
+    {
+        vibra = PlayerPrefs.GetInt("vibra") == 1;
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("vibra", vibra ? 1 : 0);
+    }
+
+    private void UpdateButtonIcon()
+    {
+        if (vibra == false)
+        {
+            vibraOn.enabled = false;
+            vibraOff.enabled = true;
+            vibraOff_Line.enabled = true;
+        }
+        else
+        {
+            vibraOn.enabled = true;
+            vibraOff.enabled = false;
+            vibraOff_Line.enabled= false;
+        }
+
+    }
+
+}
